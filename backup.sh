f11415e92f488697e8aa8d3c8b934f8b12d94677
Mark van Eijk <mark@vaneijk.co>
2021-03-20 14:06:13 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,5 +1,7 @@
 DAY=`date +"%d"`
 
+# sites
 for DIR in /var/www/!(default)/ do SITE=$(basename "$DIR"); tar -cfz /var/www/$DIR | gzip -9 | s3cmd --acl-private put - s3://%S3_BUCKET%/rocketeers/backups/%SERVER%/$SITE/$DAY.tar.gz; done
 
+# databases
 echo "SHOW DATABASES;" | mysql --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% | grep -v -E "^(Database|mysql|sys|information_schema|performance_schema)$" | while read DATABASE; do mysqldump --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% $DATABASE | gzip -9 | s3cmd --acl-private put - s3://%S3_BUCKET%/rocketeers/backups/%SERVER%/databases/$DATABASE/$DAY.sql.gz; done
