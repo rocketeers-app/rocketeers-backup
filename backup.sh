9977139610c7216dffca69e457459eaa1176645f
Mark van Eijk <mark@vaneijk.co>
2022-03-09 20:38:19 +0100

    add mysqldump options
    --add-drop-table --column-statistics=0 --extended-insert --single-transaction --skip-comments

--- backup.sh
+++ backup.sh
@@ -4,4 +4,4 @@ DAY=`date +"%d"`
 for DIR in /var/www/*; do SITE=$(basename "$DIR"); [[ $SITE = "default" ]] && continue; tar -cf - $DIR | gzip -9 | pv -L 1m -q | s3cmd --acl-private put - s3://%S3_BUCKET%/rocketeers/backups/%SERVER%/$SITE/files/$DAY.tar.gz; done
 
 # databases
-echo "SHOW DATABASES;" | mysql --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% | grep -v -E "^(Database|mysql|sys|information_schema|performance_schema)$" | while read DATABASE; do mysqldump --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% $DATABASE | gzip -9 | pv -L 1m -q | s3cmd --acl-private put - s3://%S3_BUCKET%/rocketeers/backups/%SERVER%/$DATABASE/databases/$DAY.sql.gz; done
+echo "SHOW DATABASES;" | mysql --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% | grep -v -E "^(Database|mysql|sys|information_schema|performance_schema)$" | while read DATABASE; do mysqldump --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% --add-drop-table --column-statistics=0 --extended-insert --single-transaction --skip-comments $DATABASE | gzip -9 | pv -L 1m -q | s3cmd --acl-private put - s3://%S3_BUCKET%/rocketeers/backups/%SERVER%/$DATABASE/databases/$DAY.sql.gz; done
