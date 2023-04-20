4a4e2b04054da87656bcad25b02a29c5f028098c
Mark van Eijk <mark@vaneijk.co>
2023-04-20 10:19:36 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 DAY=`date +"%d"`
+S3CMD="/usr/local/bin/s3cmd"
 
 # sites
-for DIR in /var/www/*; do SITE=$(basename "$DIR"); [[ $SITE = "default" ]] && continue; tar -cf - $DIR | gzip -9 | pv -L 1m -q | s3cmd --acl-private put - s3://rocketeers/backups/%SERVER%/$SITE/files/$DAY.tar.gz; done
+for DIR in /var/www/*; do SITE=$(basename "$DIR"); [[ $SITE = "default" ]] && continue; tar -cf - $DIR | gzip -9 | pv -L 1m -q | $S3CMD --acl-private put - s3://rocketeers/backups/%SERVER%/$SITE/files/$DAY.tar.gz; done
 
 # databases
-echo "SHOW DATABASES;" | mysql --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% | grep -v -E "^(Database|mysql|sys|information_schema|performance_schema)$" | while read DATABASE; do mysqldump --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% --add-drop-table --extended-insert --single-transaction --skip-comments $DATABASE | gzip -9 | pv -L 1m -q | s3cmd --acl-private put - s3://rocketeers/backups/%SERVER%/$DATABASE/databases/$DAY.sql.gz; done
+echo "SHOW DATABASES;" | mysql --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% | grep -v -E "^(Database|mysql|sys|information_schema|performance_schema)$" | while read DATABASE; do mysqldump --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% --add-drop-table --extended-insert --single-transaction --skip-comments $DATABASE | gzip -9 | pv -L 1m -q | $S3CMD --acl-private put - s3://rocketeers/backups/%SERVER%/$DATABASE/databases/$DAY.sql.gz; done
