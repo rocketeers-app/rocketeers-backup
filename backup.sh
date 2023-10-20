db0e69b891150993a42bf9a4b22fcf9938cb08f0
Mark van Eijk <mark@vaneijk.co>
2023-10-20 22:31:11 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@
 DAY=`date +"%d"`
 S3CMD="/usr/local/bin/s3cmd"
 
-# sites
-for DIR in /var/www/*; do SITE=$(basename "$DIR"); [[ $SITE = "default" ]] && continue; tar -cf - $DIR | gzip -9 | pv -L 1m -q | $S3CMD --acl-private put - s3://rocketeers/backups/%SERVER%/$SITE/files/$DAY.tar.gz; done
-
 # databases
 echo "SHOW DATABASES;" | mysql --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% | grep -v -E "^(Database|mysql|sys|information_schema|performance_schema)$" | while read DATABASE; do mysqldump --user=%MYSQL_USER% --password=%MYSQL_PASSWORD% --add-drop-table --extended-insert --single-transaction --skip-comments $DATABASE | gzip -9 | pv -L 1m -q | $S3CMD --acl-private put - s3://rocketeers/backups/%SERVER%/$DATABASE/databases/$DAY.sql.gz; done
+
+# sites
+for DIR in /var/www/*; do SITE=$(basename "$DIR"); [[ $SITE = "default" ]] && continue; tar -cf - $DIR/.env $DIR/certs $DIR/conf $DIR/persistent | gzip -9 | pv -L 1m -q | $S3CMD --acl-private put - s3://rocketeers/backups/%SERVER%/$SITE/files/$DAY.tar.gz; done
